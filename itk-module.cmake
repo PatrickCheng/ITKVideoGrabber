3552136e42a76c0e43f39501903c700964dde871
Patrick Cheng <pcheng@cnmc.org>
2011-08-16 20:38:03 -0400

    Setting up module structure

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,1 +1,1 @@
-itk_module(VideoGrabber DEPENDS Video-IO-Base Video-IO-OpenCV Video-Core-Common TEST_DEPENDS ITK-TestKernel)
+itk_module(Video-Grabber DEPENDS Video-IO-Base Video-IO-OpenCV Video-Core-Common TEST_DEPENDS ITK-TestKernel)
