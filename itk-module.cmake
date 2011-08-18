8763915afa119ee0293429ffe570a58837b2b1e3
Patrick Cheng <pcheng@cnmc.org>
2011-08-17 22:27:02 -0400

    Adding this external module to ITK build

---
+++ itk-module.cmake
@@ -0,0 +1,14 @@
+set(DOCUMENTATION "Video capturing using frame grabber")
+
+itk_module(Video-Grabber
+ DEPENDS
+   Video-Core-Common
+   Video-Core-OpenCV
+   Video-Filters-Common
+   Video-IO-Base
+   Video-IO-OpenCV
+   ITK-TestKernel
+   
+   DESCRIPTION
+     "${DOCUMENTATION}"
+)
