2ca8e0f6390700b71f17b6b0d8cc968ae035c452
sebastian <sebastian@sebastian-laptop.(none)>
2011-08-29 09:19:24 -0300

    added v4l2 backbone

---
+++ V4L2/include/itkV4L2VideoGrabberInterface.h
@@ -0,0 +1,156 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkV4L2VideoGrabberInterface_h
+#define __itkV4L2VideoGrabberInterface_h
+
+// Define support for V4L2 Video
+#ifndef ITK_VIDEO_USE_V4L2
+#define ITK_VIDEO_USE_V4L2
+#endif
+
+#include "itkVideoGrabberInterfaceBase.h"
+
+namespace itk
+{
+/** \class V4L2VideoGrabberInterface
+ *
+ * \brief Video grabber interface class using V4L2
+ *
+ *
+ * \ingroup Video-Grabber-V4L2
+ *
+ */
+class ITK_EXPORT V4L2VideoGrabberInterface: public VideoGrabberInterfaceBase
+{
+public:
+  /** Standard class typedefs. */
+  typedef V4L2VideoGrabberInterface       Self;
+  typedef VideoGrabberInterfaceBase          Superclass;
+  typedef SmartPointer< Self > Pointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(V4L2VideoGrabberInterface, Superclass);
+
+  /** Close the reader and writer and reset members */
+  virtual void FinishReadingOrWriting();
+
+  /** Split up the input file names -- This is public so that places where
+   * V4L2VideoGrabberInterface is used can access the individual file names. This is
+   * mostly an issue for testing. */
+  static std::vector<std::string> SplitFileNames(const char* fileList);
+
+  /*-------- This part of the interface deals with reading data. ------ */
+
+  /** Set to reading from file */
+  virtual void SetReadFromFile();
+
+  /** Set to reading from a camera */
+  virtual void SetReadFromCamera();
+
+  /** Determine the file type. Returns true if this ImageIO can read the
+   * file specified. */
+  virtual bool CanReadFile(const char *);
+
+  /** Return whether or not we can acquire video from a given device */
+  virtual bool CanReadGrabber( unsigned long cameraID );
+
+  /** Set the spacing and dimension information for the set filename. */
+  virtual void ReadImageInformation();
+
+  /** Reads the data from disk into the memory buffer provided. */
+  virtual void GrabSingleFrame(void *buffer);
+
+
+  /** Set the next frame that should be read. Return true if the operation
+   * was successful */
+  virtual bool SetNextFrameToRead(unsigned long frameNumber);
+
+  /** Accessor functions for video specific information */
+  virtual double GetPositionInMSec();
+  virtual double GetRatio();
+  virtual unsigned long GetFrameTotal();
+  virtual double GetFpS();
+  virtual unsigned long GetCurrentFrame();
+  virtual unsigned int GetIFrameInterval();
+  virtual unsigned long GetLastIFrame();
+
+  /** Override SetFileName to do parsing */
+  virtual void SetFileName(const char* fileList);
+
+  /** Override Accessors to pass through to internal image reader */
+  virtual double GetSpacing(unsigned int i) const;
+  virtual double GetOrigin(unsigned int i) const;
+  virtual std::vector< double > GetDirection(unsigned int i) const;
+
+  /*-------- This part of the interfaces deals with writing data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can write the
+   * file specified. */
+  virtual bool CanWriteFile(const char *);
+
+  /** Writes the spacing and dimentions of the image.
+   * Assumes SetFileName has been called with a valid file name. */
+  virtual void WriteImageInformation();
+
+  /** Writes the data to disk from the memory buffer provided. Make sure
+   * that the IORegion has been set properly. */
+  virtual void Write(const void *buffer);
+
+  /** Set Writer Parameters */
+  virtual void SetWriterParameters(double fps, std::vector<SizeValueType> dim, const char* fourCC,
+                                   unsigned int nChannels, IOComponentType componentType);
+
+
+
+protected:
+  V4L2VideoGrabberInterface();
+  ~V4L2VideoGrabberInterface();
+
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+  /** Reset member variables to empty state closed */
+  void ResetMembers();
+
+  /**  */
+  void OpenGrabber();
+
+  /**  */
+  void StartGrabbing();
+
+  /** Open the writer iff the reader is not open */
+  void StopGrabbing();
+
+
+private:
+  V4L2VideoGrabberInterface(const Self &);     //purposely not implemented
+  void operator=(const Self &); //purposely not implemented
+
+  /** Member Variables */
+  ImageIOBase::Pointer m_ImageIO;
+
+  /** List of files to read */
+  std::vector<std::string> m_FileNames;
+  
+
+};
+} // end namespace itk
+
+#endif // __itkV4L2VideoGrabberInterface_h
---
+++ V4L2/itk-module.cmake
@@ -0,0 +1,1 @@
+itk_module(Video-Grabber-V4L2 DEPENDS ITK-IO-Base Video-Grabber-Base TEST_DEPENDS ITK-TestKernel)
